4e63824dbbe0df5c2d5a70cce31039456b98f5a1
emilybaum <emily.r.baum@gmail.com>
2019-06-22 18:54:02 -0400

    updated typeit speed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,10 @@
 new TypeIt('#title', {
-    speed: 150,
+    speed: 100,
     startDelay: 400
 })
     .type("Hey there!")
     .pause(1000)
+    .options({ speed: 100, deleteSpeed: 75 })
     .delete(1)
     .type(", I'm Emily.")
-    // .pause(750)
-    // .options({ speed: 100, deleteSpeed: 75 })
-    // .pause(750)
-    // .type(" Check out what I'm working on.")
     .go();
